0003497856704d1996d8ddef8cc71494a569c44e
Jason Miller <contact@jasonmiller.nl>
2019-09-01 22:08:48 +0200

    Update XMonad config

--- gui/.xmonad/xmonad.hs
+++ gui/.xmonad/xmonad.hs
@@ -1,6 +1,16 @@
 import XMonad
-import XMonad.Util.Run(spawnPipe)
 import qualified Data.Map as M
+
+import XMonad.Util.Run(spawnPipe)
+
+-- Fullscreen support
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Hooks.EwmhDesktops  (ewmh, fullscreenEventHook)
+import XMonad.Layout.Fullscreen   (fullscreenManageHook)
+import Data.Maybe                 (maybeToList)
+import Control.Monad              ((>=>), join, liftM, when)
+
+import XMonad.Hooks.ManageDocks
 
 import XMonad.Config.Desktop
 import XMonad.Util.SpawnOnce
@@ -12,10 +22,21 @@ myWorkspaces   = ["web", "code", "a", "b", "c", "sfx"]
 
 myStartupHook = do
   spawnOnce "$HOME/.config/polybar/launch.sh"
+  addEWMHFullscreen 
 
-rofiDrun   = "rofi -show drun"
+myManageHook = composeAll
+    [ className =? "firefox" --> doShift "web"
+    , className =? "feh"     --> doFloat
+    , isFullscreen           --> doFullFloat
+    , fullscreenManageHook
+    ]
+
+myFocusFollowsMouse = False
+myClickJustFocuses  = False
+
+rofiDrun   = "rofi -show run"
 rofiWindow = "rofi -show window"
-rofiCalc   = "rofi -show calc -no-show-match -calc-command \"echo '{result}' | xclip\""
+rofiCalc   = "rofi -show calc -no-show-match -calc-command \"echo '{result}' | xclip\"" 
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm, xK_p), (spawn rofiDrun))
@@ -23,18 +44,34 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, xK_c), (spawn rofiCalc))
     ]
 
-myFocusFollowsMouse = False
-myClickJustFocuses  = False
-
 main = do
   xmonad $ desktopConfig
     { terminal          = myTerminal
+    , modMask           = myModMask
     , focusFollowsMouse = myFocusFollowsMouse
     , clickJustFocuses  = myClickJustFocuses
-    , modMask           = myModMask
     , borderWidth       = 0
     , workspaces        = myWorkspaces
     , startupHook       = myStartupHook <+> startupHook desktopConfig
+    , manageHook        = myManageHook <+> manageHook desktopConfig
+    , handleEventHook   = fullscreenEventHook <+> handleEventHook desktopConfig
     , keys              = \c -> myKeys c `M.union` keys XMonad.def c
     }
 
+-- Fullscreen hack for Firefox
+-- https://github.com/xmonad/xmonad-contrib/issues/183
+addNETSupported :: Atom -> X ()
+addNETSupported x   = withDisplay $ \dpy -> do
+    r               <- asks theRoot
+    a_NET_SUPPORTED <- getAtom "_NET_SUPPORTED"
+    a               <- getAtom "ATOM"
+    liftIO $ do
+       sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
+       when (fromIntegral x `notElem` sup) $
+         changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
+
+addEWMHFullscreen :: X ()
+addEWMHFullscreen   = do
+    wms <- getAtom "_NET_WM_STATE"
+    wfs <- getAtom "_NET_WM_STATE_FULLSCREEN"
+    mapM_ addNETSupported [wms, wfs]
